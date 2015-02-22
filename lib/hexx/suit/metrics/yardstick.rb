28cc7c9e20ad0a4502bcc3786f93e1396d2a14e8
Andrew Kozin <andrew.kozin@gmail.com>
2015-02-22 12:31:02 +0300

    [FEATURE] Yardstick configurator
    The configurator loads the 'yardstick' metric settings
    from the 'config/metrics/yardstick.yml' file,
    and loads the metric.
    
    == Addons
    
    * Hexx::Suit::Metrics::Yardstick

---
+++ lib/hexx/suit/metrics/yardstick.rb
@@ -0,0 +1,91 @@
+module Hexx
+  module Suit
+    module Metrics
+
+      # Prepares and runs yardstick environment from '.hexx-suit.yml'
+      class Yardstick < Base
+
+        # Loads yardstick verifier
+        #
+        # @return [Hexx::Suit::Metrics::Yardstick]
+        #
+        # @api private
+        def self.new
+          require "yardstick/rake/measurement"
+          super
+        end
+
+        # @!scope class
+        # Prepares output for the yardstick and sets the YARDSTICK_OUTPUT env
+        #
+        # @return [self]
+        def load
+          prepare_output
+          set_environment
+
+          self
+        end
+
+        # @!scope class
+        # Runs yardstick with options from '.hexx-suit.yml'
+        #
+        # @return [undefined]
+        def run
+          measurement.yardstick_measure
+        end
+
+        private
+
+        # yardstic metric definitions
+
+        def name
+          "yardstick"
+        end
+
+        def default_options
+          {
+            "output" => "tmp/yardstick/output.log",
+            "path"   => "lib/**/*.rb"
+          }
+        end
+
+        def metric
+          ::Yardstick::Rake::Measurement
+        end
+
+        # operations
+
+        def prepare_output
+          SYSTEM.call %(
+            mkdir #{ ::File.dirname(output) } -p
+            touch #{ output }
+          )
+        end
+
+        def set_environment
+          ENV["YARDSTICK_OUTPUT"] = output
+        end
+
+        # helpers
+
+        def measurement
+          metric.new(:yardstick_measure, options) do |config|
+            config.output = output
+          end
+        end
+
+        def options
+          @options ||= begin
+            list = default_options.merge settings.to_h
+            list["path"] = Array(list["path"]).join(" ")
+            list
+          end
+        end
+
+        def output
+          @output ||= options["output"]
+        end
+      end
+    end
+  end
+end
---
+++ spec/tests/lib/metrics/yardstick_spec.rb
@@ -0,0 +1,110 @@
+# encoding: utf-8
+require "spec_helper"
+require "yardstick/rake/measurement"
+
+describe Hexx::Suit::Metrics::Yardstick, :sandbox do
+
+  shared_context "without yardstick.yml" do
+
+    let(:options) do
+      { "output"  => "tmp/yardstick/output.log", "path" => "lib/**/*.rb" }
+    end
+  end
+
+  shared_context "with yardstick.yml" do
+
+    let(:options) do
+      { "output"  => "custom.log", "path" => "app/**/*.rb" }
+    end
+
+    before { prepare_settings options, "config/metrics/yardstick.yml" }
+  end
+
+  shared_examples "dependency loader" do
+
+    after { subject }
+
+    it "[loads yardstick/rake/measurement]" do
+      expect(described_class).to receive(:require)
+        .with("yardstick/rake/measurement")
+    end
+  end
+
+  shared_examples "file creator" do
+
+    before { subject }
+
+    it "[prepares output file]" do
+      expect(options["output"]).to be_present_in_sandbox
+    end
+  end
+
+  shared_examples "environment setter" do
+
+    before { ENV["YARDSTICK_OUTPUT"] = nil }
+
+    it "sets ENV[YARDSTICK_OUTPUT] to default output" do
+      expect { subject }
+        .to change { ENV["YARDSTICK_OUTPUT"] }
+        .to options["output"]
+    end
+  end
+
+  shared_examples "metric runner" do
+
+    after { subject }
+
+    it "[runs yardstick]" do
+      expect(::Yardstick::Rake::Measurement)
+        .to receive_message_chain(:new, :yardstick_measure)
+    end
+  end
+
+  describe ".load" do
+
+    subject { try_in_sandbox { described_class.load } }
+
+    context "when yardstick.yml is absent" do
+
+      include_context "without yardstick.yml"
+
+      it_behaves_like "dependency loader"
+      it_behaves_like "file creator"
+      it_behaves_like "environment setter"
+    end
+
+    context "when yardstick.yml is present" do
+
+      include_context "with yardstick.yml"
+
+      it_behaves_like "dependency loader"
+      it_behaves_like "file creator"
+      it_behaves_like "environment setter"
+    end
+  end
+
+  describe ".run" do
+
+    subject { try_in_sandbox { described_class.run } }
+
+    context "when yardstick.yml is absent" do
+
+      include_context "without yardstick.yml"
+
+      it_behaves_like "dependency loader"
+      it_behaves_like "file creator"
+      it_behaves_like "environment setter"
+      it_behaves_like "metric runner"
+    end
+
+    context "when yardstick.yml is present" do
+
+      include_context "with yardstick.yml"
+
+      it_behaves_like "dependency loader"
+      it_behaves_like "file creator"
+      it_behaves_like "environment setter"
+      it_behaves_like "metric runner"
+    end
+  end
+end
