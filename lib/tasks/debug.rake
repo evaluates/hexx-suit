0a925071f56c22aafac27cfbf42c08a71e1560fe
Andrew Kozin <andrew.kozin@gmail.com>
2015-02-22 12:43:54 +0300

    [FEATURE] $ rake debug
    The rake task runs tests with the pry rescue command.
    
    == Addons
    
    * Rake::Task['debug']

---
+++ lib/tasks/debug.rake
@@ -0,0 +1,6 @@
+caller = Hexx::Suit::Utils::System
+
+desc "Runs a test suit in a bundle environment inside pry 'rescue' wrapper."
+task :debug do
+  caller.call "bundle exec rescue rspec spec"
+end
---
+++ spec/tests/tasks/debug_spec.rb
@@ -0,0 +1,21 @@
+# encoding: utf-8
+
+describe "Rake::Task[:debug]", :tasks do
+
+  # The {#commands} variable is defined in the spec/support/config/tasks.rb
+  # It collects the list of commands, that has been sent to system by
+  # any instance of Hexx::Suit::Utils::System utility.
+
+  let(:task) { Rake::Task[:debug] }
+
+  describe ".invoke", :sandbox do
+
+    subject { try_in_sandbox { task.invoke } }
+
+    it "runs tests under pry rescue" do
+      expect { subject }.to change { commands }.to [
+        "bundle exec rescue rspec spec"
+      ]
+    end
+  end
+end
