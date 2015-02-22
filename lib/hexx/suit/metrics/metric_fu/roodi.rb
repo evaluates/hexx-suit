f872a57fe0f413a230d6756b5190a28e739a5782
Andrew Kozin <andrew.kozin@gmail.com>
2015-02-22 12:37:58 +0300

    [FEATURE] Roodi configurator
    The configurator makes the 'roodi' metric to load its settings
    from the 'config/metrics/roodi.yml' file.
    
    == Addons
    
    * Hexx::Suit::Metrics::MetricFu::Roodi

---
+++ lib/hexx/suit/metrics/metric_fu/roodi.rb
@@ -0,0 +1,32 @@
+# encoding: utf-8
+
+module Hexx
+  module Suit
+    module Metrics
+      class MetricFu < Base
+
+        # The configurer for the Roodi metric
+        class Roodi < Base
+
+          # @!scope class
+          # Changes settings source to config/metrics/roodi.yml
+          #
+          # @return [self]
+          def load
+            configure do |metric|
+              metric.roodi_config = "config/metrics/roodi.yml"
+            end
+
+            self
+          end
+
+          private
+
+          def name
+            :roodi
+          end
+        end
+      end
+    end
+  end
+end
