3e80126b64d723dbf3ae23ff83faeaaedfd056dd
Andrew Kozin <andrew.kozin@gmail.com>
2015-02-22 14:47:57 +0300

    [OTHER] spec_helper
    The spec_helper updated to properly check the coverage.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,10 @@
+# Loads the RSpec support files.
+require "rspec"
+Dir[File.expand_path "spec/support/config/*.rb"].each { |file| require file }
+
 # encoding: utf-8
+require "coveralls"
+Coveralls.wear!
 
 # Loads the code of the module with the RSpec test suit.
 require "hexx-suit"
-
-# Loads the RSpec support files.
-Dir[File.expand_path "spec/support/config/*.rb"].each { |file| require file }
-
-# Loads runtime metrics in the current scope
-ENV["USE_SIMPLECOV"] = "true"
-Hexx::Suit.load_metrics_for self
