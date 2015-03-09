e3d7ccc15e38e13138a0182d9b111c4a9a71d2df
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-09 17:12:50 +0300

    [VERSION] 0.2.0
    * MRI 1.9.3, 2.0, Rubinius 2+, JRuby 1.7+ supported as well

--- lib/hexx/suit/version.rb
+++ lib/hexx/suit/version.rb
@@ -2,6 +2,6 @@ module Hexx
   module Suit
     # The semantic version of the module.
     # @see http://semver.org/ Semantic versioning 2.0
-    VERSION = "0.1.0".freeze
+    VERSION = "0.2.0".freeze
   end # module Suit
 end # module Hexx
