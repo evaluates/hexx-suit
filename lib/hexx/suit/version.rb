4bcfb6f0d42d2119ca260af8cc042f2a0cd4c44a
Andrew Kozin <andrew.kozin@gmail.com>
2015-03-11 10:36:46 +0300

    [VERSION] 1.1.0

--- lib/hexx/suit/version.rb
+++ lib/hexx/suit/version.rb
@@ -6,7 +6,7 @@ module Hexx
 
     # The semantic version of the module.
     # @see http://semver.org/ Semantic versioning 2.0
-    VERSION = "1.0.0".freeze
+    VERSION = "1.1.0".freeze
 
   end # module Suit
 
